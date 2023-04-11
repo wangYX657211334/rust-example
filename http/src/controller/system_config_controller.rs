a2052a002cb172403c0bfa2155287e5d0c0e1fb5
姜佳祺 <jiaqi.jiang@openjawtech.com>
2023-04-11 17:57:53 +0800

    修改返回类型

--- http/src/controller/system_config_controller.rs
+++ http/src/controller/system_config_controller.rs
@@ -1,4 +1,4 @@
-use axum::{Json, http::StatusCode, response::{Response, IntoResponse}, extract::State};
+use axum::{Json, http::StatusCode, response::IntoResponse, extract::State};
 use crate::{
     models::{
         CreateSystemConfig
@@ -10,9 +10,8 @@ use crate::{
 
 
 pub async fn insert(State(app): State<ApplicationState>, Json(body): Json<CreateSystemConfig>) -> 
-        anyhow::Result<Response, ApplicationError> {
+        anyhow::Result<impl IntoResponse, ApplicationError> {
     let mut connect = app.0.get().unwrap();
     system_config_repository::insert(&mut connect, body).await?;
-    let res = StatusCode::CREATED.into_response();
-    Ok(StatusCode::CREATED.into_response())
+    Ok(StatusCode::CREATED)
 }
